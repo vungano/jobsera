81719dc90fd86bab7eac44daf7ce79e0a7fff279
vungano <53543562+vungano@users.noreply.github.com>
2024-01-15 20:20:25 +0000

    Update jobsCategoriesData.js

--- src/data/jobsCategoriesData.js
+++ src/data/jobsCategoriesData.js
@@ -9,38 +9,46 @@ import design from '../img/ux.png'
 
 export default [
     {
+        id:1,
         img: health,
         title: "Health Care",
         update: "4 new Jobs"
     },
     {
+        id:2,
         img: accounting,
         title: "Accounting",
         update: "11 new Jobs"
     },{
+        id:3,
         img: engineering,
         title: "Engineering",
         update: "3 new Jobs"
     },
     {
+        id:4,
         img: hr,
         title: "Business",
         update: "5 new Jobs"
     },{
+        id:5,
         img: marketing,
         title: "Marketing",
         update: "6 new Jobs"
     },
     {
+        id:6,
         img: teaching,
         title: "Teaching",
         update: "2 new Jobs"
     },{
+        id:7,
         img: design,
         title: "Design",
         update: "1 new Job"
     },
     {
+        id:8,
         img: misc,
         title: "Miscellaneous",
         update: "13 new Jobs"
