503662eaec6b7d43c7c952c6e3c1dc7c5403e237
vungano <53543562+vungano@users.noreply.github.com>
2024-01-15 20:23:36 +0000

    Add files via upload

---
+++ src/components/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/error.css'
+
+function Error() {
+  return (
+    <div className='error'>
+        <p>Oops, can you try to reload :|</p>
+    </div>
+  )
+}
+
+export default Error
