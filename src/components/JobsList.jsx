82559f8d39308284c503dc8ab587b9c08900ba9f
vungano <53543562+vungano@users.noreply.github.com>
2024-01-15 20:18:57 +0000

    Update JobsList.jsx

--- src/components/JobsList.jsx
+++ src/components/JobsList.jsx
@@ -2,31 +2,78 @@ import React, { useEffect, useState } from 'react'
 import FeaturedJobsItem from './FeaturedJobsItem'
 import FeaturedJobsItemSkeleton from './FeaturedJobItemSkeleton'
 import '../styles/jobslist.css'
+import '../styles/searchbar.css'
 import { Link } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import searchedJobsDummy from '../data/searchedJobsDummy'
+import Error from './Error'
+import { updateSearch } from '../actions'
 
-function JobsList() {
+var count = 0 
 
-  const searchText = useSelector(state=> state.searchReducer) 
+function JobsList({searchQuery}) {
 
-  console.log("Search text is " + searchText)
+    const searchText = useSelector(state=> state.searchReducer)
+    const dispatch = useDispatch()
+    const [localsearchText, setLocalSearchText] = useState("")
+    const [variable, setVariable] = useState(false)
+    const linkStyle = {color: 'white'};
+    const [searchedJobs, setSearchedJobs] = useState([])
+    const [error, setError] = useState(false)
+   
+  function updateSearchField(event){
+    setLocalSearchText(event.target.value)
+     }
 
-  const linkStyle = {color: 'white'};
-  const [searchedJobs, setSearchedJobs] = useState([])
+  useEffect(()=>{
+    if(searchQuery){
+      //setSearchText(searchQuery)
+      dispatch(updateSearch(searchQuery))
+      setLocalSearchText(searchQuery)
+    }
+  },[])
   
   //Function to get featured jobs
   const getSearchedJobs = async()=>{
-      const url = `https://jobsera.onrender.com/jobs/search?query=${searchText}`
-      const api = await fetch(url)
-      const data = await api.json()
-      setSearchedJobs(data)
-  }
+      try{
+        console.log('Trying to get ---------- ' + searchText)
+        const url = `https://jobsera.onrender.com/jobs/search?query=${searchText}`
+        const api = await fetch(url)
+        const data = await api.json()
+        setSearchedJobs(data)
+        console.log(data)
+        
+      }
+      catch(error){
+        ++count 
+        if(count<5){
+            getSearchedJobs()
+        }
+        else{
+            console.log("ErrOR ENCOUNTERD")
+            setError(true)
+        }
+      }
+      console.log("Count is -> " + count)
+     
+    }
 
   useEffect(()=>{
       getSearchedJobs()
   },[searchText])
 
+
+  function updateSearchReducer(){
+    dispatch(updateSearch(localsearchText))
+    console.log("SEARCHED TEXT INSIDE IS "+ localsearchText)
+    setVariable(variable => !variable)
+    setSearchedJobs([])
+    setError(false)
+    count = 0
+   
+      getSearchedJobs()
+    
+  }
 
   const searchedJobItem = searchedJobs.map(item =>{
       return <Link to={`/job_details/${item.id}`} state={linkStyle}>
@@ -40,14 +87,26 @@ function JobsList() {
           </Link>
   })
 
-  const skeletonLoaders = searchedJobsDummy.map(item =>{
+  const skeleton = searchedJobsDummy.map(item =>{
     return <FeaturedJobsItemSkeleton/>
   })
 
   return (
-        <div className="jobs_grid-3">
-            {searchedJobs.length <  1 ? skeletonLoaders : searchedJobItem}
+        <div>
+          <div className='searchBarMain'>
+            <h1>Your <span>dream job</span> <br />is just one click away.</h1>
+            <div className='search-div'>
+              <input type="text" placeholder='Job title or Keyword' value={localsearchText} onChange={updateSearchField} />
+              <Link to={`/search/${localsearchText}`}><button onClick={updateSearchReducer}>Search</button></Link>
+            </div>
+          </div>
+        
+          <div className="jobs_grid-3">
+              {(error==false && searchedJobs.length<1) && skeleton}
+              {error ? <Error/> : searchedJobItem}
+          </div>
         </div>
+
   )
 }
 
